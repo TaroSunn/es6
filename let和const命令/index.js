8c7232fe9aa2fc9f65f69ec3e06d0d6624ffe68c
TaroSunn <sunxiaomingyes@gmail.com>
2018-07-12 21:30:52 +0800

    let、const

---
+++ let和const命令/index.js
@@ -0,0 +1,12 @@
+// var a = []
+// for (let i = 0; i < 10; i++) {
+//   a[i] = function () {
+//     console.log(i)
+//   }
+// }
+// a[6]()
+
+for (let i = 0; i < 3; i++) {
+  let i = 'abc'
+  console.log(i)
+}
